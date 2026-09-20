4e4553db0d54edbbb4d008a6f21834910e448dba
Juss <286642846+jussray@users.noreply.github.com>
2026-09-20 16:01:58 -0400

    test(v0): isolate file sovereignty from source suite

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  testIgnore: 'sovereignty-file.spec.js',
   use: {
     baseURL: 'http://127.0.0.1:4173',
     trace: 'retain-on-failure',
